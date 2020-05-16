68baf3e536d20d4e52d201ba25a56c920a91a80c
Andrew Thomas <athomik79@gmail.com>
2020-05-16 12:50:36 -0400

    merging with last push

--- public/assets/js/travel.js
+++ public/assets/js/travel.js
@@ -1,101 +1,144 @@
-// TEST QUERY URL AND API KEY FOR TRIPOSO
-// var APIKey = "koerzthbb52l98zhhsdxhuovxhoouzi7";
-// var queryURL = "https://www.triposo.com/api/20200405///api/20200405/location.json?fields=id,parent_id,coordinates,score,country_id,type,intro,content,images,attribution,properties,tags,tag_labels,structured_content,public_transport_maps,name,snippet,names,musement_locations,climate,part_of,intro_language_info,structured_content_language_info,snippet_language_info" + "&units=I&key=" + APIKey;
+function activatePlacesSearch(){
+  var input = document.getElementById('search_term');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+// const locationForm = document.querySelector('location-form');
+
+// // Event Listener
+// $(function() {
+//   $("#run-search").on("click", function(event) {
+//     event.preventDefault();
+
+//   const city = document.querySelector('#search-term').value;
+
+//     console.log(location);
+//     console.log(event.target.value);
 
 
-// SEARCH BAR API QUERY BASED ON NYT EXERCISE
-function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-   var queryURL = "/api/20200405/location.json?fields=id,name,parent_id,country_id,snippet,coordinates,images";
- 
-   // Set the API key
-   var queryParams = { "api-key": "koerzthbb52l98zhhsdxhuovxhoouzi7" };
- 
-   // Grab text the user typed into the search input, add to the queryParams object
-   queryParams.q = $("#search-term")
-     .val()
-     .trim();
- 
-   // Logging the URL so we have access to it for troubleshooting
-   console.log("---------------\nURL: " + queryURL + "\n---------------");
-   console.log(queryURL + $.param(queryParams));
-   return queryURL + $.param(queryParams);
- }
- 
- function updatePage(triposoData) {
- 
-   // API doesn't have a "limit" parameter, so we have to do this ourselves
-   var numLocations = $("#location-count").val();
- 
-   console.log(triposoData);
-   console.log("------------------------------------");
- 
-   // Loop through and build elements for the defined number of locations
-   for (var i = 0; i < numLocations; i++) {
-     // Get specific info for current index
-     var location = triposoData.response.docs[i];
- 
-      // Increase the locationCount (track location # - starting at 1)
-      var locationCount = i + 1;
- 
-      // Create the  list group to contain the articles and add the article content for each
-     var $locationList = $("<ul>");
-     $locationList.addClass("list-group");
- 
-     // Add the newly created element to the DOM
-     $("#location-section").append($locationList);
-   
- 
-     // If the location has an id, log and append to $locationList
-     var name = location.name;
-     var $locationListItem = $("<li class='list-group-item locationId'>");
- 
-     if (name && name.main) {
-       console.log(name.main);
-       $locationListItem.append(
-         "<span class='label label-primary'>" +
-           locationCount +
-           "</span>" +
-           "<strong> " +
-           name.main +
-           "</strong>"
-       );
-     }
-   }
- }
- 
-     // Function to empty out the articles
- function clear() {
-   $("#location-section").empty();
- }
- 
- // CLICK HANDLERS
- // ==========================================================
- 
- // .on("click") function associated with the Search Button
- $("#run-search").on("click", function(event) {
-   // This line allows us to take advantage of the HTML "submit" property
-   // This way we can hit enter on the keyboard and it registers the search
-   // (in addition to clicks). Prevents the page from reloading on form submit.
-   event.preventDefault();
- 
-   // Empty the region associated with the articles
-   clear();
- 
-   // Build the query URL for the ajax request to the Triposo API
-   var queryURL = buildQueryURL();
- 
-   // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-   // The data then gets passed as an argument to the updatePage function
-   $.ajax({
+//     })
+
+//   $.ajax({
+//     type: "GET",
+//     url: "https://www.triposo.com/api/20200405/location.json?location_id="+city+"&count=10&fields=id,name,score,snippet&account=ORID4DVT&fields=all&token=9mzqr3x3hr2juisyaz7mfqmsezfooz85"",
+//     dataType: 'JSON',
+//     success: function(data){
+//         console.log(data);
+//     }
+//   })
+// });
+
+// $("#find").click(function(event)
+// // make initMap global and inside of that click handlers.  when run in different button clicks, pass value
+// { event.preventDefault();
+//   initMap()
+// });
+// function initMap() {
+//   let location = new Object();
+//   navigator.geolocation.getCurrentPosition(function (pos) {
+//     location.lat = pos.coords.latitude;
+//     gLat = location.lat;
+//     location.long = pos.coords.longitude;
+//     gLong = location.long;
+//     map = new google.maps.Map(document.getElementById("map"), {
+//       center: { lat: location.lat, lng: location.long },
+//       zoom: 16,
+//     });
+//     currentLat = location.lat;
+//     localStorage.setItem("lat", currentLat);
+//     currentLong = location.long;
+//     localStorage.setItem("lon", currentLong);
+//     let currentLocation = { lat: location.lat, lng: location.long };
+//     let marker = new google.maps.Marker({
+//       position: currentLocation,
+//       map: map
+//     });
+//     google.maps.event.addListener(search, 'places_changed', function () {
+//       document.getElementById("destination").onclick = function () {
+//         var input = document.getElementById("search");
+//         google.maps.event.trigger(input, "focus", {});
+//         google.maps.event.trigger(input, "keydown", { keyCode: 13 });
+//         google.maps.event.trigger(this, "focus", {});
+//       }
+//     });
+//   });
+// }
+// $(function() {
+//   $("#newburger").on("keyup", function(event) {
+//     event.preventDefault();
+//     const searchBar = document.querySelector('#newburger').addEventListener
+//     console.log(searchBar);
+//     console.log(event.target.value);
+//   })
+//   });
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $("#run-search").click(function(event) {
+    event.preventDefault();
+    var city = $("#search_term").val().charAt(0).toUpperCase() + $("#search_term").val().slice(1)
+    console.log(city)
+    if (city) {
+      var queryURL = "https://www.triposo.com/api/20200405/poi.json?location_id="+city+"&count=10&fields=id,name,score,snippet&account=ORID4DVT&fields=all&token=9mzqr3x3hr2juisyaz7mfqmsezfooz85";
+  $.ajax({
      url: queryURL,
      method: "GET"
-   }).then(updatePage);
- });
- 
- //  .on("click") function associated with the clear button
- $("#clear-all").on("click", clear);
-
+   }).then(response => {
+       console.log(response);
+   });
+    }
+  });
+  $("#newburger").on("keyup", function(event) {
+    event.preventDefault();
+    const searchBar = document.querySelector('#newburger').addEventListener
+    console.log(searchBar);
+    console.log(event.target.value);
+  })
+  // $(".create-form").on("submit", function(event) {
+  //   event.preventDefault();
+  $("#wish").on("click", function(event) {
+    event.preventDefault();
+    var newBurger = {
+      burger_name: $("#search_term")
+        .val()
+        .trim(),
+      devoured: 0
+    };
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("Added new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+  $(".eatburger").on("click", function(event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+    var devouredState = {
+      devoured: 1
+    };
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState
+    }).then(function() {
+      console.log("Burger devoured", devouredState);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+  $(".trashburger").on("click", function(event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+    // Send the DELETE request.
+    $.ajax({
+      type: "DELETE",
+      url: "/api/burgers/" + id
+    }).then(location.reload());
+  });
+});
 
 
 //  CURRENT LOCATION MAP FUNCTION
